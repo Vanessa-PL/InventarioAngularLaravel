1f9476159534fbe7ee3e83cc3ebe502e27deb1f2
vane <vanessapabuena37@gmail.com>
2024-06-19 13:24:58 -0500

    Se realizo la configuracion de la base de datos y se creo el archivo de api, el modelo y el controlador de los productos

---
+++ database/migrations/2024_06_19_170346_create_productos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('CodigoProducto');
+            $table->string('Nombre');
+            $table->integer('Cantidad');
+            $table->decimal('PrecioUnitario', 10, 2);
+            $table->decimal('Total', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\productoController;
+
+//Create
+Route::post('/productos', function () {
+    return 'Crear poductos';
+});
+
+//Read
+Route::get('/productos', [productoController::class, 'index']);
+
+//Read
+Route::get('/productos/{id}', function () {
+    return 'Ver producto';
+});
+
+//Update
+Route::put('/productos/{id}', function () {
+    return 'Actualizar productos';
+});
+
+//Delete
+Route::delete('/productos/{id}', function () {
+    return 'Eliminar productos';
+});
